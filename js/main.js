3309bb8e47c12e3bf406c76aea125d6d10e782c2
vsTeFFxD <99238973+vsTeFFxD@users.noreply.github.com>
2022-10-17 15:45:33 +0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 const nav = document.querySelector("#nav");
 const navBtn = document.querySelector("#nav-btn");
 
+const arr = [
+  (navLink1 = document.querySelector("#nav-link1")),
+  (navLink2 = document.querySelector("#nav-link2")),
+  (navLink3 = document.querySelector("#nav-link3")),
+];
+
 navBtn.onclick = () => {
   console.log("Кнопка нажата!");
   nav.classList.toggle("open");
 };
+
+arr.forEach(function (navLink) {
+  console.log(navLink);
+  console.log(arr);
+  navLink.onclick = () => {
+    console.log("Click");
+    nav.classList.remove("open");
+  };
+});
+
+// navLink.onclick = () => {
+//   console.log("Click");
+//   nav.classList.remove("open");
+// };
 
 AOS.init({
   once: true,
